5ceb7be83f916a937bfaa4a7052042f339637b88
Ashish Sharma <ashish.sharma@emory.edu>
2016-01-13 18:10:18 -0500

    stride code uploaded
    The example that was shown in class, illustrating the benefits of using
    stride and layout to reduce latency and optimize execution

---
+++ Examples/DataLayout/mem2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(int argc, char **argv)
+{
+    int64_t size = 10000;
+    int64_t i, j;
+    srand(time(NULL));
+    static int A[10000][10000];
+    for(i=0; i<size; i++) {
+        for(j=0; j<size; j++) {
+            A[i][j] = rand()%10;
+        }
+    }
+    static int column_sum[10000];
+    clock_t begin, end;
+    double time_spent;
+
+    begin = clock();
+// Exploiting data layout to calculate column_sum
+    for(i = 0; i < size; i++)
+    column_sum[i] = 0;
+    for(i = 0; i < size; i++)
+      for(j = 0; j < size; j++)
+        column_sum[j] += A[i][j];
+    end = clock();
+
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Time Spent: %f\n", time_spent);
+}
